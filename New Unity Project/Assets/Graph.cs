366a31d749f5003a2fb4ab423cb8e601bb2b0571
ppsavila <ppsavila2@gmail.com>
2019-09-28 15:06:20 -0300

    cu

--- New Unity Project/Assets/Graph.cs
+++ New Unity Project/Assets/Graph.cs
@@ -1,73 +1,130 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Xml.Serialization;
+using System.IO;
 
 public class Graph : MonoBehaviour
 {
     public static Graph instance;
-    public float Grain = 1.0f;
-    [Range(0,5)]
-    public float Radius = 1f;
-    public int Size  = 15;
+    [Range(0, 0)]
+    public float Grain;
+    [Range(0, 5)]
+    public float GrainFactor;
+    [Range(0, 5)]
+    public float Radius;
+    [Range(0, 5)]
+    public float Slope;
+    public int Size = 15;
     public GameObject Node;
     public Transform nodes;
-    public GameObject[,] nodesMatriz;
+    public Node[,] nodesMatriz;
+    public List<Node> nodesList = new List<Node>();
     int gridSize;
+    float nodeDiametre;
 
-    public void Awake ()
+
+    void Awake()
     {
         instance = this;
-        GenerateGraph2();
+
+    }
+
+    private void Start()
+    {
+        nodeDiametre = Radius * 2;
+        Grain = (nodeDiametre / GrainFactor);
+        gridSize = Mathf.RoundToInt((Size * Size) / Grain);
+
+    }
+
+
+    private void FixedUpdate()
+    {
+
+    }
+
+    public void salvarGrafo()
+    {
+        XmlSerializer serializador = new XmlSerializer(typeof(List<Node>));
+        StreamWriter arqDados = new StreamWriter("Grafo.xml");
+        serializador.Serialize(arqDados.BaseStream, nodesList);
+        arqDados.Close();
+    }
+
+    public void carregarGrafo()
+    {
+        XmlSerializer serializador = new XmlSerializer(typeof(List<Node>));
+        StreamReader arqLeit = new StreamReader("Grafo.xml");
+        nodesList = (List<Node>)serializador.Deserialize(arqLeit.BaseStream);
+        arqLeit.Close();
+
+
+
+    }
+
+    void instanciarNovos()
+    {
+        for (int i = 0; i < nodes.childCount; i++)
+        {
+            Destroy(nodes.GetChild(i));
+        }
+        RaycastHit ray;
+        foreach (Node node in nodesList)
+        {
+            if (Physics.Raycast(new Vector3(node.posX, 0, node.posY), Vector3.down, out ray, 100.0f))
+            {
+                Instantiate(Node, ray.point, Quaternion.identity, nodes);
+            }
+        }
     }
 
 
 
     public void GenerateGraph2()
     {
-        float nodeDiametre = Radius * 2;
-        gridSize = Mathf.RoundToInt(Size *  Grain);
-
-        nodesMatriz = new GameObject[gridSize, gridSize];
+        nodesMatriz = new Node[gridSize, gridSize];
         Vector3 worldBottomLeft = transform.position - Vector3.right * Size / 2 - Vector3.forward * Size / 2;
 
         RaycastHit ray;
 
-        for (int i = 0; i < gridSize; i++)
+        for (int i = 0; i <= gridSize; i++)
         {
-            for (int j = 0; j < gridSize; j++)
+            for (int j = 0; j <= gridSize; j++)
             {
-                Vector3 worldPoint = worldBottomLeft + Vector3.right * (i * nodeDiametre ) + Vector3.forward * (j * nodeDiametre );
-
-
+                Vector3 worldPoint = (worldBottomLeft + Vector3.right * (i * Grain) + Vector3.forward * (j * Grain));
                 if (Physics.Raycast(worldPoint, Vector3.down, out ray, 100.0f))
                 {
-                    GameObject aux = Instantiate(Node, ray.point, Quaternion.identity,nodes);
-                    nodesMatriz[i, j] = aux;
-                    
+                    GameObject aux = Instantiate(Node, ray.point, Quaternion.identity, nodes);
                     aux.GetComponent<Node>().posX = i;
                     aux.GetComponent<Node>().posY = j;
-
-
-
-                    
+                    nodesList.Add(aux.GetComponent<Node>());
                 }
             }
         }
+
+        foreach (Node node in nodesList)
+        {
+            node.vizinhos = nodeVizinhos(node);
+        }
     }
 
-    public List<GameObject> nodeVizinhos(GameObject node, int gX,int gY)
+    public List<Node> nodeVizinhos(Node node)
     {
-        List<GameObject> vizinhos = new List<GameObject>();
+        List<Node> vizinhos = new List<Node>();
         for (int x = -1; x <= 1; x++)
         {
             for (int y = -1; y <= 1; y++)
             {
                 if (x == 0 & y == 0)
                     continue;
-                int checkX = gX + x;
-                int checkY = gY + y;
+                float checkX = node.posX + x;
+                float checkY = node.posY + y;
                 if (checkX >= 0 && checkX < gridSize && checkY >= 0 && checkY < gridSize)
-                    vizinhos.Add(nodesMatriz[checkX, checkY]);
+                {
+                    vizinhos.Add(nodesList.Find(z => z.posX == checkX && z.posY == checkY));
+                }
+
             }
         }
         return vizinhos;
@@ -76,21 +133,20 @@ public class Graph : MonoBehaviour
 
     private void OnDrawGizmos()
     {
-        Gizmos.DrawWireCube(this.transform.position,new Vector3(Size,1,Size));
-
-        foreach (GameObject node in nodesMatriz) // que isso gente 
+        Gizmos.DrawWireCube(this.transform.position, new Vector3(Size, 1, Size));
+        Gizmos.color = Color.red;
+        foreach (Node node in nodesList)
         {
-            //Codigo super leve 
-            List<GameObject> vizinhos = nodeVizinhos(node, node.GetComponent<Node>().posX, node.GetComponent<Node>().posY);
-
-            if(vizinhos!= null)
+            foreach (Node vizinho in node.vizinhos)
             {
-                foreach (GameObject vizinho in vizinhos)
+                if (vizinho != null)
                 {
-                    Gizmos.DrawLine(node.transform.position, vizinho.transform.position); // cu
+                    Debug.DrawLine(node.transform.position, vizinho.transform.position);
                 }
             }
-            
         }
+
+
     }
+
 }
--- New Unity Project/Assets/Node.cs
+++ New Unity Project/Assets/Node.cs
@@ -7,9 +7,9 @@ public class Node : MonoBehaviour
     public bool walkable;
     public bool ligarVerifacaodecolisao;
     public LayerMask dontWalk;
-    public int posX;
-    public int posY;
-
+    public List<Node> vizinhos = new List<Node>();
+    public float posX;
+    public float posY;
     public float gCost;
     public float hCost;
     
@@ -24,7 +24,7 @@ public class Node : MonoBehaviour
         return gCost + hCost;
     }
 
-    private void Update()
+    private void FixedUpdate()
     {
         if (walkable)
             GetComponent<Renderer>().material.color = Color.green;
@@ -33,15 +33,30 @@ public class Node : MonoBehaviour
 
         if (!ligarVerifacaodecolisao)
         {
-            if (Physics.CheckSphere(this.transform.position, Graph.instance.Radius, dontWalk))
-            {
-                walkable = false;
-            }
-            else
-            {
-                walkable = true;
-            }
+           
+                if (Physics.CheckSphere(this.transform.position, Graph.instance.Radius, dontWalk) && testeVizinhos())
+                {
+                    walkable = false;
+                }
+                else
+                {
+                    walkable = true;
+                }
         }
+        posX = transform.position.x;
+        posY = transform.position.z;
+    }
+
+    bool testeVizinhos()
+    {
+        foreach (Node vizinho in vizinhos)
+        {
+            if ((this.transform.position.y - vizinho.transform.position.y) < Graph.instance.Slope)
+                return false;
+            else
+                return true;
+        }
+        return false;
     }
 
     private void OnDrawGizmosSelected()
