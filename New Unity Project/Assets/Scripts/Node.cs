90651e13a4170f3e21a46809bac04f9c609ffd44
ppsavila <ppsavila2@gmail.com>
2019-09-28 14:27:46 -0300

    sei lamano

--- New Unity Project/Assets/Scripts/Node.cs
+++ New Unity Project/Assets/Scripts/Node.cs
@@ -7,24 +7,20 @@ public class Node : MonoBehaviour
     public bool walkable;
     public bool ligarVerifacaodecolisao;
     public LayerMask dontWalk;
-    public int posX;
-    public int posY;
+    public List<Node> vizinhos = new List<Node>();
+    public float posX;
+    public float posY;
 
     public float gCost;
     public float hCost;
     
-
-    public void setRadius(float radius)
-    {
-        GetComponent<SphereCollider>().radius = radius;
-    }
 
     public float fCost()
     {
         return gCost + hCost;
     }
 
-    private void Update()
+    private void FixedUpdate()
     {
         if (walkable)
             GetComponent<Renderer>().material.color = Color.green;
@@ -33,7 +29,7 @@ public class Node : MonoBehaviour
 
         if (!ligarVerifacaodecolisao)
         {
-            if (Physics.CheckSphere(this.transform.position, Graph.instance.Radius, dontWalk))
+            if (Physics.CheckSphere(this.transform.position, Graph.instance.Radius, dontWalk) && testeVizinhos())
             {
                 walkable = false;
             }
@@ -42,7 +38,23 @@ public class Node : MonoBehaviour
                 walkable = true;
             }
         }
+            posX = transform.position.x;
+            posY = transform.position.z;
     }
+
+    bool testeVizinhos()
+    {
+        foreach (Node vizinho in vizinhos)
+        {
+            if ((this.transform.position.y - vizinho.transform.position.y) < Graph.instance.Slope)
+                return false;
+            else
+                return true;
+        }
+        return false;
+    }
+
+
 
     private void OnDrawGizmosSelected()
     {
