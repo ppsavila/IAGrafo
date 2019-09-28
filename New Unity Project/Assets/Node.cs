37df28d61dea4006ed0489fa6eb1ae30308fc409
ppsavila <ppsavila2@gmail.com>
2019-09-28 10:08:17 -0300

    FIZ AI

---
+++ New Unity Project/Assets/Node.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node : MonoBehaviour
+{
+    public bool walkable;
+    public bool ligarVerifacaodecolisao;
+    public LayerMask dontWalk;
+    public int posX;
+    public int posY;
+
+    public float gCost;
+    public float hCost;
+    
+
+    public void setRadius(float radius)
+    {
+        GetComponent<SphereCollider>().radius = radius;
+    }
+
+    public float fCost()
+    {
+        return gCost + hCost;
+    }
+
+    private void Update()
+    {
+        if (walkable)
+            GetComponent<Renderer>().material.color = Color.green;
+        else
+            GetComponent<Renderer>().material.color = Color.red;
+
+        if (!ligarVerifacaodecolisao)
+        {
+            if (Physics.CheckSphere(this.transform.position, Graph.instance.Radius, dontWalk))
+            {
+                walkable = false;
+            }
+            else
+            {
+                walkable = true;
+            }
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawWireSphere(this.transform.position, Graph.instance.Radius);
+    }
+}
+
--- New Unity Project/Assets/Scripts/Grid.cs
+++ New Unity Project/Assets/Scripts/Grid.cs
@@ -1,170 +1,170 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEditor;
+﻿//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+//using UnityEditor;
 
-public class Grid : MonoBehaviour
-{
-    public Transform player;
-    public Node[,] grid; // matriz de nodes
-    public Vector2 gridWorldSize; // tamanho do grid
-    public float nodeRadius; // Raio do node
-    public LayerMask unWalkableMask; // Layer de não andavel
-    
-    [Range(0,3)]
-    public float Slope=0.2f;
+//public class Grid : MonoBehaviour
+//{
+//    public Transform player;
+//    public Node[,] grid; // matriz de nodes
+//    public Vector2 gridWorldSize; // tamanho do grid
+//    public float nodeRadius; // Raio do node
+//    public LayerMask unWalkableMask; // Layer de não andavel
 
-    float nodeDiametre; // diametro do node 
-    int gridSizeX, gridSizeY; 
+//    public GameObject spherePrefab;
 
-    private void Start()
-    {
-        nodeDiametre = nodeRadius * 2;
-        gridSizeX = Mathf.RoundToInt(gridWorldSize.x / nodeDiametre); // cria a quantidade de nodes possiveis de serem criados no eixo x 
-        gridSizeY = Mathf.RoundToInt(gridWorldSize.y / nodeDiametre); // cria a quantidade de nodes possiveis de serem criados no eixo z 
-        CreateGrid();
-        DrawSphereGizmo();
-    }
+//    [Range(0,3)]
+//    public float Slope=0.2f;
 
-    /// <summary>
-    /// Retorna todos os vizinhos de um determinado node 
-    /// </summary>
-    /// <param name="node"> o node </param>
-    /// <returns> lista de vizinhos </returns>
-    public List<Node> nodeVizinhos(Node node)
-    {
-        List<Node> vizinhos = new List<Node>();
-        for (int x = -1; x <=1 ; x++)
-        {
-            for (int y = -1; y <= 1; y++)
-            {
-                if (x == 0 & y == 0)
-                    continue;
-                int checkX = node.gridX + x;
-                int checkY = node.gridY + y;
-                if (checkX >= 0 && checkX < gridSizeX && checkY >= 0 && checkY < gridSizeY)
-                    vizinhos.Add(grid[checkX,checkY]);
-            }
-        }
-        return vizinhos;
-    }
+//    float nodeDiametre; // diametro do node 
+//    int gridSizeX, gridSizeY; 
 
-    /// <summary>
-    /// Cria o grid
-    /// </summary>
-    void CreateGrid()
-    {
+//    private void Start()
+//    {
+//        nodeDiametre = nodeRadius * 2;
+//        gridSizeX = Mathf.RoundToInt(gridWorldSize.x / nodeDiametre); // cria a quantidade de nodes possiveis de serem criados no eixo x 
+//        gridSizeY = Mathf.RoundToInt(gridWorldSize.y / nodeDiametre); // cria a quantidade de nodes possiveis de serem criados no eixo z 
+//        CreateGrid();
+//    }
 
-        grid = new Node[gridSizeX, gridSizeY]; // crio nosso grid com o numero maximo de node em cada eixo
-        Vector3 worldBottomLeft = transform.position - Vector3.right*gridWorldSize.x/2 - Vector3.forward*gridWorldSize.y/2; // me retorna o canto esquerdo do nosso grid
-        RaycastHit ray;
-        bool walkable =false;
+//    /// <summary>
+//    /// Retorna todos os vizinhos de um determinado node 
+//    /// </summary>
+//    /// <param name="node"> o node </param>
+//    /// <returns> lista de vizinhos </returns>
+//    public List<Node> nodeVizinhos(Node node)
+//    {
+//        List<Node> vizinhos = new List<Node>();
+//        for (int x = -1; x <=1 ; x++)
+//        {
+//            for (int y = -1; y <= 1; y++)
+//            {
+//                if (x == 0 & y == 0)
+//                    continue;
+//                int checkX = node.gridX + x;
+//                int checkY = node.gridY + y;
+//                if (checkX >= 0 && checkX < gridSizeX && checkY >= 0 && checkY < gridSizeY)
+//                    vizinhos.Add(grid[checkX,checkY]);
+//            }
+//        }
+//        return vizinhos;
+//    }
 
-        for (int x = 0; x < gridSizeX; x++)
-        {
-            for (int y = 0; y < gridSizeY; y++)
-            {
+//    /// <summary>
+//    /// Cria o grid
+//    /// </summary>
+//    void CreateGrid()
+//    {
+
+//        grid = new Node[gridSizeX, gridSizeY]; // crio nosso grid com o numero maximo de node em cada eixo
+//        Vector3 worldBottomLeft = transform.position - Vector3.right*gridWorldSize.x/2 - Vector3.forward*gridWorldSize.y/2; // me retorna o canto esquerdo do nosso grid
+//        RaycastHit ray;
+//        bool walkable =false;
+
+//        for (int x = 0; x < gridSizeX; x++)
+//        {
+//            for (int y = 0; y < gridSizeY; y++)
+//            {
                 
-                Vector3 worldPoint = worldBottomLeft + Vector3.right * (x * nodeDiametre + nodeRadius) + Vector3.forward * (y * nodeDiametre + nodeRadius) + Vector3.down;// me retorna cada ponto no mundo
+//                Vector3 worldPoint = worldBottomLeft + Vector3.right * (x * nodeDiametre + nodeRadius) + Vector3.forward * (y * nodeDiametre + nodeRadius) + Vector3.down;// me retorna cada ponto no mundo
 
-                if (Physics.Raycast(worldPoint,Vector3.up,out ray,unWalkableMask))
-                {
-                    if (ray.collider.gameObject.tag != "Player")
-                        worldPoint.y = 1 + ray.point.y;
-                    else
-                        worldPoint.y = 0;
-                }
-                else
-                {
-                    worldPoint.y = 0;
-                }
-
-                walkable = !(Physics.CheckSphere(worldPoint, nodeRadius, unWalkableMask)); // checo usando o chekSphere se colidiu com algum obstaculo setando assim a variavel walkable como true ou false
-                grid[x, y] = new Node(walkable, worldPoint,x,y); // Crio o nove na cordena x,y passando o bool se é walkable ou nao e passando tambem sua posição no mundo
-            }
-        }
-
-        foreach(Node node in grid)
-        {
-            List<Node> vizinhos = nodeVizinhos(node);
-
-            foreach (Node nodeV in vizinhos)
-            {
-                float distY = nodeV.worldPosition.y - node.worldPosition.y;
-                if (distY > Slope)
-                    nodeV.walkable = false;
-            }
-        }
-
-    }
-
-    /// <summary>
-    /// Retorno um node apartir de uma posição do mundo 
-    /// </summary>
-    /// <param name="worldPosition">posição </param>
-    /// <returns>Node</returns>
+//                if (Physics.Raycast(worldPoint,Vector3.up,out ray,unWalkableMask))
+//                {
+//                    Debug.DrawLine(worldPoint, Vector3.up);
 
 
-    public Node nodeFromWorldPoint(Vector3 worldPosition) 
-    {
-        float percenteX = (worldPosition.x + gridWorldSize.x / 2) / gridWorldSize.x;
-        float percenteY = (worldPosition.z + gridWorldSize.y / 2) / gridWorldSize.y;
-        percenteX = Mathf.Clamp01(percenteX);
-        percenteY = Mathf.Clamp01(percenteY);
-        int x = Mathf.RoundToInt((gridSizeX - 1) * percenteX);
-        int y = Mathf.RoundToInt((gridSizeY - 1) * percenteY);
-        return grid[x, y];
-    }
+//                    if (ray.collider.gameObject.tag != "Player")
+//                        worldPoint.y = 1 + ray.point.y;
+//                    else
+//                        worldPoint.y = 0;
+//                }
+//                else
+//                {
+//                    worldPoint.y = 0;
+//                }
+
+//                walkable = !(Physics.CheckSphere(worldPoint, nodeRadius, unWalkableMask)); // checo usando o chekSphere se colidiu com algum obstaculo setando assim a variavel walkable como true ou false
+//                grid[x, y] = new Node(walkable, worldPoint,x,y); // Crio o nove na cordena x,y passando o bool se é walkable ou nao e passando tambem sua posição no mundo
+//                instantiateSphere(grid[x, y]);
+
+//            }
+//        }
+
+//        foreach(Node node in grid)
+//        {
+//            List<Node> vizinhos = nodeVizinhos(node);
+
+//            foreach (Node nodeV in vizinhos)
+//            {
+//                float distY = nodeV.worldPosition.y - node.worldPosition.y;
+//                if (distY > Slope)
+//                    nodeV.walkable = false;
+//            }
+//        }
+
+//    }
+
+//    /// <summary>
+//    /// Retorno um node apartir de uma posição do mundo 
+//    /// </summary>
+//    /// <param name="worldPosition">posição </param>
+//    /// <returns>Node</returns>
 
 
-    /// <summary>
-    /// Serve pra desenhar no view da unity um cubo parar mostrar a area do grid
-    /// </summary>
-   
-    void DrawSphereGizmo()
-    {
-      
-    }
-
-    [DrawGizmo(GizmoType.Pickable | GizmoType.Active)]
-    private void OnDrawGizmos()
-    {
-        Gizmos.DrawWireCube(transform.position, new Vector3(gridWorldSize.x, 1, gridWorldSize.y)); // bota o cubo no tamanho total do grid
+//    public Node nodeFromWorldPoint(Vector3 worldPosition) 
+//    {
+//        float percenteX = (worldPosition.x + gridWorldSize.x / 2) / gridWorldSize.x;
+//        float percenteY = (worldPosition.z + gridWorldSize.y / 2) / gridWorldSize.y;
+//        percenteX = Mathf.Clamp01(percenteX);
+//        percenteY = Mathf.Clamp01(percenteY);
+//        int x = Mathf.RoundToInt((gridSizeX - 1) * percenteX);
+//        int y = Mathf.RoundToInt((gridSizeY - 1) * percenteY);
+//        return grid[x, y];
+//    }
 
 
+//    void instantiateSphere(Node node)
+//    {
+        
+//        GameObject obj = Instantiate(spherePrefab, node.worldPosition, Quaternion.identity);
+//        obj.transform.localScale *= nodeRadius;
+//        if (node.walkable)
+//            obj.GetComponent<Renderer>().material.color = Color.green;
+//        else
+//            obj.GetComponent<Renderer>().material.color = Color.red;
 
-        if (grid != null)
-        {
-            Node playerNode = nodeFromWorldPoint(player.position);
+        
 
-            foreach (Node node in grid)
-            {
-                List<Node> vizinhos = nodeVizinhos(node);
-
-                foreach (Node nodeV in vizinhos)
-                {
-
-                    if (node.walkable && nodeV.walkable)
-                        Gizmos.color = Color.blue;
-                    else
-                        Gizmos.color = Color.black;
-                    Gizmos.DrawLine(node.worldPosition, nodeV.worldPosition);
-                }
+//    }
 
 
-                if (node.walkable)
-                {
-                    if (playerNode == node) // colore onde o player esta
-                        Gizmos.color = Color.cyan;
-                    else
-                        Gizmos.color = Color.green;
-                }
-                else
-                    Gizmos.color = Color.red;
+//    /// <summary>
+//    /// Serve pra desenhar no view da unity um cubo parar mostrar a area do grid
+//    /// </summary>
+//    private void OnDrawGizmos()
+//    {
+//        Gizmos.DrawWireCube(transform.position, new Vector3(gridWorldSize.x, 1, gridWorldSize.y)); // bota o cubo no tamanho total do grid
 
-                Gizmos.DrawSphere(node.worldPosition, nodeDiametre - .7f);
-                
-            }
-        }  // bota as esferas no mapa
-    }
-}
+
+
+//        if (grid != null)
+//        {
+//            Node playerNode = nodeFromWorldPoint(player.position);
+
+//            foreach (Node node in grid)
+//            {
+//                List<Node> vizinhos = nodeVizinhos(node);
+
+//                foreach (Node nodeV in vizinhos)
+//                {
+
+//                    if (node.walkable && nodeV.walkable)
+//                        Gizmos.color = Color.blue;
+//                    else
+//                        Gizmos.color = Color.black;
+//                    Gizmos.DrawLine(node.worldPosition, nodeV.worldPosition);
+//                } 
+//            }
+//        }  // bota as esferas no mapa
+//    }
+//}
